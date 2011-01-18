3a24ead177d68728a107b17b116db3dafed65b20
Michael C. Harris <michael@twofishcreative.com>
2011-01-18 15:59:42 +1100

    Create testsuite elements for each group and wrap them all in a testsuites element. Correct the attribute names, because the junit format is a work of wonder and mystery.

--- extensions/test/templates/console/stats.xml.php
+++ extensions/test/templates/console/stats.xml.php
@@ -3,56 +3,59 @@
 
 	$dom = new DOMDocument('1.0', 'UTF-8');
 	$dom->formatOutput = true;
-	$testsuite = $dom->createElement('testsuite');
+	$testsuites = $dom->createElement('testsuites');
 	if ($self->title) {
-		$testsuite->setAttribute('name', $self->title);
+		$testsuites->setAttribute('name', $self->title);
 	}
 
 	// Grab the totals and add attributes to the testsuite
-  $stats = $self->stats();
+	$stats = $self->stats();
 	$count = $stats['count'];
 	$attrs = array(
-		'asserts' => intval($count['asserts']) ?: 0,
-		'fails' => intval($count['fails']) ?: 0,
-		'exceptions' => intval($count['exceptions']) ?: 0,
+		'tests' => intval($count['asserts']) ?: 0,
+		'failures' => intval($count['fails']) ?: 0,
+		'errors' => intval($count['exceptions']) ?: 0,
 	);
 
 	foreach ($attrs as $attr => $value) {
-		$testsuite->setAttribute($attr, $value);
+		$testsuites->setAttribute($attr, $value);
 	}
 
 	// Iterate over the test results
-	// @todo Why 'group' an array of tests? Other tests?
-	foreach ($self->results['group'][0] as $result) {
-		$testcase = $dom->createElement('testcase');
-		$testcase->setAttribute('classname', $result['class']);
-		$testcase->setAttribute('name', $result['method']);
-		if ($result['result'] == 'fail' || $result['result'] == 'exception') {
-			$error_element = null;
-			$element = '';
-			$message = "{$result['class']}::{$result['method']}()\n";
-			$message .= "{$result['message']}\n";
-			if ($result['result'] == 'fail') {
-				$element = 'failure';
-				$type = 'failed';
-				$message .= "Expected {$result['data']['expected']} but got {$result['data']['result']}\n";
-				$message .= "{$result['file']}:{$result['line']}\n";
-			} elseif ($result['result'] == 'exception') {
-				$element = 'error';
-				$type = 'Exception';
-				$message .= "{$result['file']}:{$result['line']}\n";
-				if (isset($result['trace']) && !empty($result['trace'])) {
-					$message .= "Trace: {$result['trace']}\n";
+	foreach ($self->results['group'] as $group) {
+		$testsuite = $dom->createElement('testsuite');
+		foreach ($group as $result) {
+			$testcase = $dom->createElement('testcase');
+			$testcase->setAttribute('classname', $result['class']);
+			$testcase->setAttribute('name', $result['method']);
+			if ($result['result'] == 'fail' || $result['result'] == 'exception') {
+				$error_element = null;
+				$element = '';
+				$message = "{$result['class']}::{$result['method']}()\n";
+				$message .= "{$result['message']}\n";
+				if ($result['result'] == 'fail') {
+					$element = 'failure';
+					$type = 'failed';
+					$message .= "Expected {$result['data']['expected']} but got {$result['data']['result']}\n";
+					$message .= "{$result['file']}:{$result['line']}\n";
+				} elseif ($result['result'] == 'exception') {
+					$element = 'error';
+					$type = 'Exception';
+					$message .= "{$result['file']}:{$result['line']}\n";
+					if (isset($result['trace']) && !empty($result['trace'])) {
+						$message .= "Trace: {$result['trace']}\n";
+					}
 				}
+				$error_element = $dom->createElement($element, $message);
+				$error_element->setAttribute('type',$type);
+				$testcase->appendChild($error_element);
 			}
-			$error_element = $dom->createElement($element, $message);
-			$error_element->setAttribute('type',$type);
-			$testcase->appendChild($error_element);
+			$testsuite->appendChild($testcase);
 		}
-		$testsuite->appendChild($testcase);
+		$testsuites->appendChild($testsuite);
 	}
 
-	$dom->appendChild($testsuite);
+	$dom->appendChild($testsuites);
 
 	echo $dom->saveXML();
 ?>
